c18431949220b4b47c72064de4976ffa7bd9b518
Iago Fonteles <iagofonteles1@gmail.com>
2025-10-22 20:08:29 -0300

    first commit

---
+++ Runtime/ISearchSettings.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BlueGravity
+{
+    public interface ISearchSettings
+    {
+        string Title { get; }
+        IEnumerable GetItens(object target);
+        string GetName(object item);
+    }
+
+    public interface ISearchSettings<T> : ISearchSettings
+    {
+        new IEnumerable<T> GetItens(object target);
+        string GetName(T obj);
+
+        IEnumerable ISearchSettings.GetItens(object target) => GetItens(target);
+        string ISearchSettings.GetName(object item) => GetName((T)item);
+    }
+}
