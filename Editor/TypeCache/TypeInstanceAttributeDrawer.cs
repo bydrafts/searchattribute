89e153d721da236f0ad6238a6e296f03a8ad1fde
Iago Fonteles <iagofonteles1@gmail.com>
2026-05-13 17:45:27 -0300

    fix type drawer

--- Editor/TypeCache/TypeInstanceAttributeDrawer.cs
+++ Editor/TypeCache/TypeInstanceAttributeDrawer.cs
@@ -23,26 +23,25 @@ namespace Drafts.Editor {
             var currValue = property.managedReferenceValue;
             var currType = currValue?.GetType();
 
+            var text = fieldInfo.FieldType.IsArray ? "" : label.text + ": ";
+            text += currType?.Name ?? "null";
+            label.text = " ";
+            
             EditorGUIUtility.labelWidth *= 1.2f;
-            if (currValue == null) {
-                DrawButton(position, property, fieldType, "null");
-                if (fieldInfo.FieldType.IsArray) label.text = null;
-                var hasLabel = label != GUIContent.none && !string.IsNullOrEmpty(label.text);
-                var rect = position;
-                rect.height = EditorGUIUtility.singleLineHeight;
-                if (hasLabel) EditorGUI.PrefixLabel(rect, GUIUtility.GetControlID(FocusType.Passive), label);
-            } else {
-                RemoveButton(position, property, currType.Name);
-                if (fieldInfo.FieldType.IsArray) label.text = "";
+            if (currValue == null)
+                DrawButton(position, property, fieldType, text);
+            else {
+                RemoveButton(position, property, text);
                 EditorGUI.PropertyField(position, property, label, true);
             }
             EditorGUIUtility.labelWidth /= 1.2f;
         }
 
         private void RemoveButton(Rect pos, SerializedProperty property, string text) {
-            var w = EditorGUIUtility.labelWidth;
-            pos.x += fieldInfo.FieldType.IsArray ? 0 : w / 2;
-            pos.width = fieldInfo.FieldType.IsArray ? w : w / 2;
+            //var w = EditorGUIUtility.labelWidth;
+            //pos.x += fieldInfo.FieldType.IsArray ? 0 : w / 2;
+            //pos.width = fieldInfo.FieldType.IsArray ? w : w / 2;
+            pos.width = EditorGUIUtility.labelWidth;
             pos.height = EditorGUIUtility.singleLineHeight;
 
             if (!GUI.Button(pos, text)) return;
@@ -51,9 +50,7 @@ namespace Drafts.Editor {
         }
 
         private void DrawButton(Rect pos, SerializedProperty property, Type fieldType, string text) {
-            var w = EditorGUIUtility.labelWidth;
-            pos.x += fieldInfo.FieldType.IsArray ? 0 : w / 2;
-            pos.width = fieldInfo.FieldType.IsArray ? w : w / 2;
+            pos.width = EditorGUIUtility.labelWidth;
             pos.height = EditorGUIUtility.singleLineHeight;
 
             if (!GUI.Button(pos, text)) return;
